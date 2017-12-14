559ee12f0921c707c55d3899295fba20012b715b
Sumit Yadav <sumit.yadav@blueoptima.com>
2017-12-15 00:34:48 +0530

    Laying out initial project structure

---
+++ src/main/java/com/sample/app/ExampleServlet.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright by sumit yadav
+ */
+
+package com.sample.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Sumit Yadav on 15/12/17.
+ */
+public class ExampleServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 8914090542308271379L;
+
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    PrintWriter writer = resp.getWriter();
+    writer.println("Hello World");
+  }
+}
